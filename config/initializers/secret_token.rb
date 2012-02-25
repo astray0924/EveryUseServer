6513354d07938a0419b090d41bedb501f8a8d177
KyoungRok Jang <astray0924@gmail.com>
2012-02-25 22:19:22 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiUse::Application.config.secret_token = '98ce3b292a226b14f63ba23659fd62fec75abac02465b4c48089ee9590454a976cc82515f56d8f2162041bf3bb52fd97a6178bf6938974a3f33fabf15ad59ff7'
