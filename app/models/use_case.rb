2d74abc052d2e29ce8e0e6c280f206132f6ec4c5
KyoungRok Jang <astray0924@gmail.com>
2012-03-04 01:02:51 +0900

    added validation for photos

--- app/models/use_case.rb
+++ app/models/use_case.rb
@@ -4,4 +4,8 @@ class UseCase < ActiveRecord::Base
   has_attached_file :photo, :styles => {:thumb => "100x100#", :large => "400x400>"},
                     :url => "/:attachment/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/:attachment/:id/:style/:basename.:extension"
+                    
+  # validates_attachment_presence :photo
+  # validates_attachment_size :photo, :less_than => 5.megabytes 
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/bmp']
 end
