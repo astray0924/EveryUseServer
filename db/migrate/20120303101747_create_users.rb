17308abb078deccc3f10f988c03f78828dbc1495
KyoungRok Jang <astray0924@gmail.com>
2012-03-03 19:25:00 +0900

    finished setting up the models for auth generated controllers for auth

--- db/migrate/20120303101747_create_users.rb
+++ db/migrate/20120303101747_create_users.rb
@@ -1,6 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string    :email,               :null => false
+      t.string    :name,                :null => false
+      
+      # required
+      t.string    :persistence_token,   :null => false
+      
+      # Optional
+      t.string    :crypted_password,    :null => false
+      t.string    :password_salt,       :null => false
+      t.integer   :login_count,         :null => false, :default => 0
 
       t.timestamps
     end
