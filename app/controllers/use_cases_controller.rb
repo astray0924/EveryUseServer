071aa4c1ac6450687c0a266551efd6b42b80e27a
Jang, Kyoung Rok <astray0924@gmail.com>
2012-07-21 15:36:30 +0900

    test

--- app/controllers/use_cases_controller.rb
+++ app/controllers/use_cases_controller.rb
@@ -136,14 +136,7 @@ class UseCasesController < ApplicationController
 
   # newn API
   def groups
-    page, limit = get_pagination_params(params)
-    type = params[:type]
-
-    groups = UseCase.all.group_by("item")
-
-    respond_to do |format|
-      format.json { render json: groups }
-    end
+    
   end
 
   private
