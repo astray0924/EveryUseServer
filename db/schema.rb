a00e84dc97deaaaf8eacabd723944ef6033ea19e
Jang, Kyoung Rok <astray0924@gmail.com>
2012-08-10 22:51:42 +0900

    sdfsf

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20120810133816) do
   create_table "use_cases", :force => true do |t|
     t.string   "item"
     t.string   "purpose"
+    t.string   "purpose_type"
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
     t.integer  "user_id"
@@ -55,7 +56,6 @@ ActiveRecord::Schema.define(:version => 20120810133816) do
     t.integer  "favorites_count",    :default => 0,  :null => false
     t.integer  "wows_count",         :default => 0,  :null => false
     t.integer  "metoos_count",       :default => 0,  :null => false
-    t.string   "purpose_type",       :default => ""
     t.string   "place",              :default => ""
   end
 
