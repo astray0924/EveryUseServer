41f3d8d43a28f2e904dc56eeb7f690d6c79b9f46
Kyoung-Rok Jang <astray0924@gmail.com>
2012-11-07 21:35:57 +0900

    trivial

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    @gallery_list = UseCase.includes(:user).order(:created_at).limit(10)
+    @gallery_list = UseCase.includes(:user).order(:created_at).limit(30)
     @wow_list = UseCase.order('wows_count DESC').includes(:user).limit(5) 
     @metoo_list = UseCase.order('metoos_count DESC').includes(:user).limit(5) 
 
