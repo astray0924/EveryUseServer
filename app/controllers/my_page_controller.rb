6d7ab75e0245a0d26e2348403dabb8c1a6ddd6db
KyoungRok <astray0924@gmail.com>
2013-01-24 10:53:31 +0900

    My Page 하는중

--- app/controllers/my_page_controller.rb
+++ app/controllers/my_page_controller.rb
@@ -1,6 +1,5 @@
 class MyPageController < ApplicationController
   before_filter :require_user
-  
   def shared
     @title = "Shared"
     @page, @limit = get_pagination_params(params)
@@ -8,7 +7,7 @@ class MyPageController < ApplicationController
     if !current_user
       @use_cases = []
     else
-      @use_cases = UseCase.includes(:wow, :metoo, :favorite).where('user_id = ?', current_user.id).paginate(:page => @page, :per_page => @limit)
+      @use_cases = current_user.use_cases.paginate(:page => @page, :per_page => @limit)
     end
 
     respond_to do |format|
@@ -18,9 +17,32 @@ class MyPageController < ApplicationController
   end
 
   def commented
+    @user = current_user
+    @use_cases = Hash.new
+    @use_cases[:wow] = @user.wow.order('id DESC').map{ |x| x.use_case }
+    @use_cases[:metoo] = @user.metoo.order('id DESC').map{ |x| x.use_case }
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @use_cases }
+    end
   end
 
   def scrapped
+    @title = "Scrapped"
+    @page, @limit = get_pagination_params(params)
+
+    if !current_user
+      @use_cases = []
+    else
+      @favorites = current_user.favorite.order('id DESC')
+      @use_cases = @favorites.map{ |x| x.use_case }.paginate(:page => @page, :per_page => @limit)
+    end
+
+    respond_to do |format|
+      format.html { render :template => 'my_page/use_case_list' }
+      format.json { render json: @use_cases }
+    end
   end
 
   def following
