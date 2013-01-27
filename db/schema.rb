c1ec0995a1d09a7508ff017aad1aeaf09ac5f6f3
unknown <user@Jang-PC.(none)>
2013-01-27 10:17:37 +0900

    sdfsdf

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130126185123) do
   create_table "use_cases", :force => true do |t|
     t.string   "item"
     t.string   "purpose"
+    t.string   "purpose_type",       :default => ""
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
     t.integer  "user_id"
@@ -55,7 +56,6 @@ ActiveRecord::Schema.define(:version => 20130126185123) do
     t.integer  "favorites_count",    :default => 0,  :null => false
     t.integer  "wows_count",         :default => 0,  :null => false
     t.integer  "metoos_count",       :default => 0,  :null => false
-    t.string   "purpose_type",       :default => ""
     t.string   "place",              :default => ""
     t.string   "lang",               :default => ""
   end
